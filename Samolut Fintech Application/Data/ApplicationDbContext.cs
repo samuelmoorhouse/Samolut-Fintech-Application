def1311178203c4514f15081f65f6efd05d8af75
Sam Home PC <sam.moorhouse@icloud.com>
2026-02-08 15:27:32 +0000

    Add project files.

---
+++ Samolut Fintech Application/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Samolut_Fintech_Application.Models.DatabaseModels;
+namespace Samolut_Fintech_Application.Data
+{
+
+//this will connect the c sharp to the db tables. 
+    public class ApplicationDbContext: DbContext
+    {
+        //takes settings like the db name and passes it to the system
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+             
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Samolut Fintech Application/Models/DatabaseModels/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace Samolut_Fintech_Application.Models.DatabaseModels
+{
+    public class Customer
+    {
+        public Guid id { get; set; }
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public int phonenumber { get; set; }
+
+        public string password { get; set; }
+
+
+    }
+}
