bc1e482bc4f9ff3ceb356a2f93724644920c9ca1
Sam Home PC <sam.moorhouse@icloud.com>
2026-02-18 15:50:36 +0000

    Login Functionality done with sessions.

--- Samolut Fintech Application/Controllers/AccountController.cs
+++ Samolut Fintech Application/Controllers/AccountController.cs
@@ -38,7 +38,7 @@ namespace Samolut_Fintech_Application.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(string phonenumber, string password)
         {
-            var user = await _context.Customer.FirstOrDefaultAsync(u => u.PHONE_NUMBER == phonenumber && u.PASSWORD == password);   //first or default is like fetch assoc in php and the u is c sharps like for i. Have to use async version.
+            var user = await _context.Customer.FirstOrDefaultAsync(u => u.PHONE_NUMBER.Trim() == phonenumber.Trim() && u.PASSWORD.Trim() == password.Trim());   //first or default is like fetch assoc in php and the u is c sharps like for i. Have to use async version.
             
             if (user == null) { 
                 ViewBag.ErrorMessage = "Invalid phonenumber or password."; //this ViewBag is for how i would echo in php. ill style it later.
--- Samolut Fintech Application/Program.cs
+++ Samolut Fintech Application/Program.cs
@@ -6,6 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+//all session stuff
+builder.Services.AddSession(options => {
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
@@ -24,6 +31,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseSession();
 
 app.UseRouting();
 
