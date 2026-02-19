b4dea5d4db3aa7b90dce9d14a85e75112cd1f772
Sam Home PC <sam.moorhouse@icloud.com>
2026-02-19 10:14:58 +0000

    small fix

--- Samolut Fintech Application/Controllers/HomeController.cs
+++ Samolut Fintech Application/Controllers/HomeController.cs
@@ -90,27 +90,41 @@ namespace Samolut_Fintech_Application.Controllers
             //so my user id from the session
             int? userId = HttpContext.Session.GetInt32("UserId");
             //get list of accounts for dropdown
-            var accounts = await _context.Account
-                .Where(i => i.CUSTOMER_ID == userId)
+            var accounts = await _context.Account.Where(i => i.CUSTOMER_ID == userId)
                 .ToListAsync();
 
             //make list if Currencies as in my db i made it so the accounts like currency name like GBP was in a seperate table so its 3nf
             var currencies = await _context.CurrentCurrency.ToListAsync();
             //find the balance that matches the selected account in the html select tag
-            var balance = accounts.Where(i=> i.ACCOUNT_ID == selectedAccountId).Select(i=> i.ACCOUNT_BALANCE).FirstOrDefault();
+            var balance = accounts.Where(i=> i.ACCOUNT_ID == selectedAccountId)
+                .Select(i=> i.ACCOUNT_BALANCE)
+                .FirstOrDefault();
 
             //need currency icon so it looks cool
             //i need in currencies table  where currency id on the slected account and select icon.
-            var currencyId = accounts.Where(i => i.ACCOUNT_ID == selectedAccountId).Select(i => i.COUNTRY_CURRENCY_ID).FirstOrDefault();
-            var currencyIcon = currencies.Where(i => i.COUNTRY_CURRENCY_ID == currencyId).Select(i => i.CURRENCY_ICON).FirstOrDefault();
+            var currencyId = accounts.Where(i => i.ACCOUNT_ID == selectedAccountId)
+                .Select(i => i.COUNTRY_CURRENCY_ID)
+                .FirstOrDefault();
 
-            
+            var currencyIcon = currencies.Where(i => i.COUNTRY_CURRENCY_ID == currencyId)
+                .Select(i => i.CURRENCY_ICON)
+                .FirstOrDefault();
+
+            var transactions = await _context.Transaction.Where(i => i.SENDER_ACCOUNT_ID == selectedAccountId || i.RECEIVER_ACCOUNT_ID == selectedAccountId)
+                .OrderByDescending(i => i.TRANSACTION_TIME)
+                .ToListAsync();
+
+            var selectedName = accounts.Where(i => i.ACCOUNT_ID == selectedAccountId)
+                .FirstOrDefault();
+
 
 
             ViewBag.Currencies = currencies;
             ViewBag.balance = balance; //puts the id thats in the html select tag in viewbag so i can change balance and transactions and stuff
             ViewBag.currencyIcon = currencyIcon;
             ViewBag.selected = selectedAccountId;
+            ViewBag.Transactions = transactions;
+            ViewBag.slectedName = selectedName;
 
             return View(accounts);
         }
@@ -121,16 +135,29 @@ namespace Samolut_Fintech_Application.Controllers
             {
                 return RedirectToAction("Login", "Account");
             }
+
+            int? usserId = HttpContext.Session.GetInt32("UserId");
+
+
+
+
+
+
             return View();
 
         }
 
-        public IActionResult Payments()
+        public async Task<IActionResult> Payments()
         {
             if (HttpContext.Session.GetInt32("UserId") == null)
             {
                 return RedirectToAction("Login", "Account");
             }
+
+
+
+
+
 
 
             return View();
